956e4fcd0941f469de6b1ffa379e5e2ee5c15809
aadagilis <adagilis@asu.edu>
2016-05-31 09:56:26 -0700

    Update 2

--- CSE360Assignment2/src/cse360assign2/TestAnalytics.java
+++ CSE360Assignment2/src/cse360assign2/TestAnalytics.java
@@ -14,18 +14,18 @@ public class TestAnalytics {
 	{
 		System.out.println("Enter a list of integers. When done, press a character.");
 		Scanner scan = new Scanner(System.in);
-        Analytics list = new Analytics();
-        int num;
-        while(scan.hasNextInt())
-        {
-            num = scan.nextInt();
-            list.insert(num);
-        }
-        System.out.println("Mean: " + list.mean());
-        System.out.println("Median: " + list.median());
-        System.out.println("High: " + list.high());
-        System.out.println("Low: " + list.low());
-        System.out.println("Number of Ints: " + list.numInts());
+		Analytics list = new Analytics();
+		int num;
+		while(scan.hasNextInt())
+		{
+			num = scan.nextInt();
+			list.insert(num);
+		}
+		System.out.println("Mean: " + list.mean());
+		System.out.println("Median: " + list.median());
+		System.out.println("High: " + list.high());
+		System.out.println("Low: " + list.low());
+		System.out.println("Number of Ints: " + list.numInts());
 	}
 
 }
